44319436f2359775e160748188bd603aa4dd1fb0
Nicolas Albert <nicolas.albert@gmail.com>
2019-12-17 13:49:03 +0100

    remove from documentation removed part of the administration

--- index.php
+++ index.php
@@ -7,15 +7,14 @@ if ($_GET["v"]) {
 
 	$versions = array();
 
-	try{
-        for ($i=0; $i < count($cleanFolders); $i++){
-            if(is_dir($cleanFolders[$i]))
-                array_push($versions,$cleanFolders[$i]);
-        }
-		header("Content-Type: application/json");
-		echo json_encode($versions, JSON_FORCE_OBJECT);
-	}
-	catch(E_WARNING $e){
+	try {
+		for ($i=0; $i < count($cleanFolders); $i++){
+			if (is_dir($cleanFolders[$i]))
+				array_push($versions,$cleanFolders[$i]);
+			}
+			header("Content-Type: application/json");
+			echo json_encode($versions, JSON_FORCE_OBJECT);
+	} catch(E_WARNING $e) {
 		echo "Can't read folders ".$e;
 	}
 } else {
--- js/docv.js
+++ js/docv.js
@@ -1,89 +1,88 @@
 var docVersion;
 $(document).ready(function () {
-    getAllVersions();
-    docVersion = $(".fa-home")[0].href.replace(new RegExp(".*/(.*?)/"), "$1");
-    handleGitHubUrl();
+	getAllVersions();
+	docVersion = $(".fa-home")[0].href.replace(new RegExp(".*/(.*?)/"), "$1");
+	handleGitHubUrl();
 });
 
 // This function creates the correct url to link to the file's page on github
-function handleGitHubUrl(){
-    let ver = ".*/"+ getCurrentVersion();
-    let cur = "/" + getCurrentVersion();
-    if($("#objclass")[0]){
-        let objclass = $("#objclass")[0].value;
-        let newurl = $("#ghlink")[0].href;
-         // Build the url properly to match the repository
-        newurl = newurl.replace("convertigo-doc","convertigo");
+function handleGitHubUrl() {
+	let ver = ".*/" + getCurrentVersion();
+	let cur = "/" + getCurrentVersion();
+	if ($("#objclass")[0]) {
+		let objclass = $("#objclass")[0].value;
+		let newurl = $("#ghlink")[0].href;
+		 // Build the url properly to match the repository
+		newurl = newurl.replace("convertigo-doc","convertigo");
 
-        if(location.href.includes("mobile-application/components")){
-            if(objclass == "ion_objects.json")
-                newurl += "/develop/engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/ion_objects.json";
-            else{
-                objclass = objclass.replace(new RegExp(/[^a-zA-Z]/g),"/");
-                newurl += "/develop/engine/src/" + objclass + ".properties";
-            }
-            $("#ghlink")[0].href = newurl;
-        }
-        else{
-            objclass = objclass.replace(new RegExp(/[^a-zA-Z]/g),"/");
-            objclass = objclass.replace(new RegExp(/(.*?\/beans\/(.*)\/)/gi),"$1res/");                  
-            newurl += "/develop/engine/src/" + objclass + ".properties";
-            $("#ghlink")[0].href = newurl;
-        }
-    }
-    else{
-        $("#ghlink")[0].href += location.pathname.replace(new RegExp(ver),cur);
-        if(location.pathname.includes("index.html"))
-            $("#ghlink")[0].href += $("#ghlink")[0].href.replace("index.html","index.md");
-        else
-            $("#ghlink")[0].href += "index.md";
-    }
+		if (location.href.includes("mobile-application/components")) {
+			if (objclass == "ion_objects.json") {
+				newurl += "/develop/engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/ion_objects.json";
+			} else {
+				objclass = objclass.replace(new RegExp(/[^a-zA-Z]/g),"/");
+				newurl += "/develop/engine/src/" + objclass + ".properties";
+			}
+			$("#ghlink")[0].href = newurl;
+		} else {
+			objclass = objclass.replace(new RegExp(/[^a-zA-Z]/g),"/");
+			objclass = objclass.replace(new RegExp(/(.*?\/beans\/(.*)\/)/gi),"$1res/");
+			newurl += "/develop/engine/src/" + objclass + ".properties";
+			$("#ghlink")[0].href = newurl;
+		}
+	} else {
+		$("#ghlink")[0].href += location.pathname.replace(new RegExp(ver),cur);
+		if (location.pathname.includes("index.html")) {
+			$("#ghlink")[0].href += $("#ghlink")[0].href.replace("index.html","index.md");
+		} else {
+			$("#ghlink")[0].href += "index.md";
+		}
+	}
 }
 
 function getCurrentVersion() {
-    return docVersion;
+	return docVersion;
 }
 
-function getAllVersions(){
-    $.getJSON($(".fa-home")[0].href + "../index.php","v=k").done(function(res){
-        createComboBoxWithJSON(res);
-    });    
+function getAllVersions() {
+	$.getJSON($(".fa-home")[0].href + "../index.php","v=k").done(function(res) {
+		createComboBoxWithJSON(res);
+	});	
 }
 
-function createComboBoxWithJSON(json){
-    if(json == "")
-        console.log("Can't read the JSON"); 
-    else{
-        let select = document.createElement("select");
-        select.id = "vselect";
-        select.className = "navselect";
+function createComboBoxWithJSON(json) {
+	if (json == "") {
+		console.log("Can't read the JSON"); 
+	} else {
+		let select = document.createElement("select");
+		select.id = "vselect";
+		select.className = "navselect";
 
-        for(var i in json){
-            let value = json[i];
-            if (value != "master") {
-               let option = document.createElement("option");
-               option.textContent = json[i];
-               if(json[i] == getCurrentVersion()) {
-                  option.setAttribute("selected","selected");
-               }
-               select.options.add(option);
-            }
-        }
+		for (var i in json) {
+			let value = json[i];
+			if (value != "master") {
+				let option = document.createElement("option");
+				option.textContent = json[i];
+				if (json[i] == getCurrentVersion()) {
+					option.setAttribute("selected", "selected");
+				}
+				select.options.add(option);
+			}
+		}
 
-        let option = document.createElement("option");
-        option.textContent = "older";
-        select.options.add(option);
+		let option = document.createElement("option");
+		option.textContent = "older";
+		select.options.add(option);
 
-        $(select).on("change", function () {
-            let currVersion = getCurrentVersion();
-            let selectedVersion = select.selectedOptions[0].innerHTML;
-            if (selectedVersion == "older") {
-                location.href = "/document/all/";
-            } else {
-                location.href = location.href.replace("/" + currVersion + "/", "/" + selectedVersion + "/");
-            }
-        });
-        let versionDiv = document.getElementById('versions');
-        versionDiv.appendChild(select);
-    }
+		$(select).on("change", function () {
+			let currVersion = getCurrentVersion();
+			let selectedVersion = select.selectedOptions[0].innerHTML;
+			if (selectedVersion == "older") {
+				location.href = "/document/all/";
+			} else {
+				location.href = location.href.replace("/" + currVersion + "/", "/" + selectedVersion + "/");
+			}
+		});
+		let versionDiv = document.getElementById('versions');
+		versionDiv.appendChild(select);
+	}
 }
